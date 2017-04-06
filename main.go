f71671ada6ca015d77883fa0d3f07d5be94f6f17
sappharx <vinnysisk@gmail.com>
2017-04-05 20:17:29 -0600

    move openGL related stuff to its own file

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	//"math/rand"
 	"runtime"
-	"strings"
 	"time"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
@@ -22,22 +18,6 @@ const (
 	fps	= 10
 
 	threshold	= 0.15
-
-	vertexShaderSource = `
-		#version 410
-		in vec3 vp;
-		void main() {
-			gl_Position = vec4(vp, 1.0);
-		}
-	` + "\x00"
-
-	fragmentShaderSource = `
-		#version 410
-		out vec4 frag_colour;
-		void main() {
-			frag_colour = vec4(1, 1, 1, 1);
-		}
-	` + "\x00"
 )
 
 func main() {
@@ -77,100 +57,4 @@ func draw(cells [][]*cell, window *glfw.Window, program uint32) {
 
 	glfw.PollEvents()
 	window.SwapBuffers()
-}
-
-/**
- * Initializes glfw and returns a Window to use
- */
-func initGlfw() *glfw.Window {
-	if err := glfw.Init(); err != nil {
-		panic(err)
-	}
-
-	glfw.WindowHint(glfw.Resizable, glfw.False)
-	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-
-	window, err := glfw.CreateWindow(width, height, "Conway's Game of Life", nil, nil)
-
-	if err != nil {
-		panic(err)
-	}
-
-	window.MakeContextCurrent()
-
-	return window
-}
-
-/**
- * Initializes OpenGL and returns an initialized program
- */
-func initOpenGL() uint32 {
-	if err := gl.Init(); err != nil {
-		panic(err)
-	}
-
-	version := gl.GoStr(gl.GetString(gl.VERSION))
-	log.Println("OpenGL version", version)
-
-	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
-	if err != nil {
-		panic(err)
-	}
-
-	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
-
-	prog := gl.CreateProgram()
-	gl.AttachShader(prog, vertexShader)
-	gl.AttachShader(prog, fragmentShader)
-	gl.LinkProgram(prog)
-
-	return prog
-}
-
-/**
- * Initializes and returns a vertex array object from the provided points
- */
-func makeVao(points []float32) uint32 {
-	var vbo uint32	// vertex buffer object
-
-	gl.GenBuffers(1, &vbo)
-	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	gl.BufferData(gl.ARRAY_BUFFER, 4 * len(points), gl.Ptr(points), gl.STATIC_DRAW)
-
-	var vao uint32
-
-	gl.GenVertexArrays(1, &vao)
-	gl.BindVertexArray(vao)
-	gl.EnableVertexAttribArray(0)
-	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
-
-	return vao
-}
-
-func compileShader(source string, shaderType uint32) (uint32, error) {
-	shader := gl.CreateShader(shaderType)
-
-	csources, free := gl.Strs(source)
-	gl.ShaderSource(shader, 1, csources, nil)
-	free()
-	gl.CompileShader(shader)
-
-	var status int32
-	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
-
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
-
-		logMsg := strings.Repeat("\x00", int(logLength + 1))
-		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(logMsg))
-
-		return 0, fmt.Errorf("failed to compile %v: %v", source, logMsg)
-	}
-
-	return shader, nil
 }
